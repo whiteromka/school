4eed3875c9f8c09ec4c2369e46ba088ac2a335c1
Rom <whiteromka@yandex.ru>
2026-03-21 12:34:01 +0300

    css фиксы

--- resources/views/components/nexus/php-blocks.blade.php
+++ resources/views/components/nexus/php-blocks.blade.php
@@ -34,22 +34,27 @@
                      data-users-count="{{ $module->openActiveModule?->users->count() ?? 0 }}"
                      data-max-users="{{ $max }}">
 
-                    <div class="service-card-count-line-wrap" data-context="Заполненность группы в %">
-                        <div class="service-card-count-line">
-                            {{-- блоки .item будут добавлены через JS --}}
-                        </div>
-                        <span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M320 16a104 104 0 1 1 0 208 104 104 0 1 1 0-208zM96 88a72 72 0 1 1 0 144 72 72 0 1 1 0-144zM0 416c0-70.7 57.3-128 128-128 12.8 0 25.2 1.9 36.9 5.4-32.9 36.8-52.9 85.4-52.9 138.6l0 16c0 11.4 2.4 22.2 6.7 32L32 480c-17.7 0-32-14.3-32-32l0-32zm521.3 64c4.3-9.8 6.7-20.6 6.7-32l0-16c0-53.2-20-101.8-52.9-138.6 11.7-3.5 24.1-5.4 36.9-5.4 70.7 0 128 57.3 128 128l0 32c0 17.7-14.3 32-32 32l-86.7 0zM472 160a72 72 0 1 1 144 0 72 72 0 1 1 -144 0zM160 432c0-88.4 71.6-160 160-160s160 71.6 160 160l0 16c0 17.7-14.3 32-32 32l-256 0c-17.7 0-32-14.3-32-32l0-16z"/></svg>
-                        </span>
-
-                    </div>
-{{--                    @if($module->openActiveModule)--}}
-{{--                        Записалось: {{ $module->openActiveModule->users->count() }}--}}
-{{--                        Макс количество: {{ $max }} тут всегда 90  --}}
-{{--                    @endif--}}
+                    {{--                            <div class="user-counter" data-context="Записалось на бесплатную часть">--}}
+                    {{--                                <?php for($x = 1; $x <= 90; $x++) :?>--}}
+                    {{--                                <div class="item"></div>--}}
+                    {{--                                <?php endfor;?>--}}
+                    {{--                            </div>--}}
 
                     <div class="service-index">
-                        <div class="left"> 0<?= $i ?></div>
+                        <div class="left">
+                            <span>0<?= $i ?></span>
+                            <div class="service-card-count-line-wrap" data-context="Записалось на бесплатную часть">
+                                <div class="service-card-count-line">
+                                    {{-- блоки .item будут добавлены через JS --}}
+                                </div>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="icon-grey">
+                                        <path d="M320 16a104 104 0 1 1 0 208 104 104 0 1 1 0-208zM96 88a72 72 0 1 1 0 144 72 72 0 1 1 0-144zM0 416c0-70.7 57.3-128 128-128 12.8 0 25.2 1.9 36.9 5.4-32.9 36.8-52.9 85.4-52.9 138.6l0 16c0 11.4 2.4 22.2 6.7 32L32 480c-17.7 0-32-14.3-32-32l0-32zm521.3 64c4.3-9.8 6.7-20.6 6.7-32l0-16c0-53.2-20-101.8-52.9-138.6 11.7-3.5 24.1-5.4 36.9-5.4 70.7 0 128 57.3 128 128l0 32c0 17.7-14.3 32-32 32l-86.7 0zM472 160a72 72 0 1 1 144 0 72 72 0 1 1 -144 0zM160 432c0-88.4 71.6-160 160-160s160 71.6 160 160l0 16c0 17.7-14.3 32-32 32l-256 0c-17.7 0-32-14.3-32-32l0-16z"/>
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+
                         <div class="right">
                             <div>
                                 <span><span class="orange_"><?= $module->lesson_price ?> RUR</span> за урок</span>
@@ -59,7 +64,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="service-name">
+                    <div class="service-name" style="display: inline-block">
                             <?= $module->name ?>
                         <span class="dark-grey"> / </span>
                         <span class="orange"> <?= $module->module_price ?> RUR </span>
@@ -125,8 +130,8 @@
 @push('scripts')
     <script>
         document.addEventListener('DOMContentLoaded', function () {
-            const ITEM_WIDTH = 5;      // ширина блока .item в px
-            const GAP_WIDTH = 4;       // gap между блоками в px
+            const ITEM_WIDTH = 3;      // ширина блока .item в px
+            const GAP_WIDTH = 2;       // gap между блоками в px
             const TOTAL_PER_ITEM = ITEM_WIDTH + GAP_WIDTH; // 9px на один блок
             const MAX_USERS = 90;      // максимальное количество пользователей
 
